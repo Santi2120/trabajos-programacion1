8313effe169390c9f925e3bd97cf06876723ec44
Santi2120 <santino2648@gmail.com>
2025-09-21 11:36:23 -0300

    trabajo practico listas

---
+++ TP listas/TpListas.py
@@ -0,0 +1,26 @@
+#ejercicio 1
+
+multiplos_de_4 = list(range(4, 101, 4))
+print(multiplos_de_4)
+
+#ejercicio 2
+
+lista_objetos = ["tenis", "formula1" ,"cafe" ,"programacion", "futbol"]
+print(lista_objetos[-2])
+
+#ejercicio 3
+lista_vacia = []
+lista_vacia.append("tenis")
+lista_vacia.append("formula 1")
+lista_vacia.append("futbol")
+
+print(lista_vacia)
+
+#ejercicio 4
+
+animales = ["perro", "gato", "conejo", "pez"]
+
+animales[1] = "loro"
+animales[3] = "oso"
+
+print(animales)
